24ea5ab568f75141da8d46fd4f10dd557e4a9526
Eduardo Cardoso <ygarasab@gmail.com>
2021-09-23 00:57:07 -0300

    add our packages

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+      origin: 'null',
+      methods: ["GET", "POST"]
+    }
+  });
+
+io.on("connection", (socket) => {
+    console.log('Someone Connected')
+});
+
+httpServer.listen(80);
