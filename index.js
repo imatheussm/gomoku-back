eab8156738644c89a21c8af567f9a49c04613959
Eduardo Cardoso <ygarasab@gmail.com>
2021-09-27 23:38:44 -0300

    simple enough room system

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
+
+let rooms = {}
+
 const io = new Server(httpServer, {
     cors: {
       origin: '*',
@@ -12,6 +15,22 @@ const io = new Server(httpServer, {
   });
 io.on("connection", (socket) => {
     console.log('Someone Connected')
+    socket.on('join', room => {
+        socket.join(room)
+        if(!(room in rooms))
+        {
+            rooms[room] = 1
+            socket.emit('joined', {player:0})
+        }
+        else if(rooms[room])
+        {
+            rooms[room] = 0
+            socket.emit('joined', {player:1})
+        }
+        else
+            socket.emit('full')
+    })
+    socket.on('play', data => socket.rooms.forEach(room => io.to(room).emit('play', data)))
 });
 
 httpServer.listen(80);
