495a25175aae2a6031b76e8389a2578d90f27fb1
Eduardo Cardoso <ygarasab@gmail.com>
2021-09-27 23:53:01 -0300

    added some loggin

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const fs = require('fs')
+const util = require('util')
+
+let log_file = fs.createWriteStream('/log/gomoku.log', {flags : 'w'});
+console.log = d =>  log_file.write(util.format(d) + '\n')
 
 const app = express();
 const httpServer = createServer(app);
@@ -14,8 +19,9 @@ const io = new Server(httpServer, {
     }
   });
 io.on("connection", (socket) => {
-    console.log('Someone Connected')
+    console.log(`[ ${new Date().toLocaleString()} ] Someone connected`)
     socket.on('join', room => {
+        console.log(`[ ${new Date().toLocaleString()} ] Someone is trying to join room ${room}`)
         socket.join(room)
         if(!(room in rooms))
         {
@@ -30,7 +36,11 @@ io.on("connection", (socket) => {
         else
             socket.emit('full')
     })
-    socket.on('play', data => socket.rooms.forEach(room => io.to(room).emit('play', data)))
+    socket.on('play', data => {
+        console.log(`[ ${new Date().toLocaleString()} ] A play was sent at room ${socket.rooms[0]} by player ${data.player}`)
+        socket.rooms.forEach(room => io.to(room).emit('play', data))
+    })
 });
 
-httpServer.listen(80);
+console.log(`[ ${new Date().toLocaleString()} ] Starting`)
+httpServer.listen(3002);
